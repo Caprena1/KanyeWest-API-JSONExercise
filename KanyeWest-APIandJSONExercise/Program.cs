0a4ee6629090b2a7c0a64e399595245ab58498b8
Caprena Richie <robcaprena@gmail.com>
2021-10-26 18:46:15 -0400

    first commit

---
+++ KanyeWest-APIandJSONExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeRest_APIandJSONExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HttpClient();
+                     
+                      
+            for (int i = 0; i < 5; i++)
+            {
+                var kanyeResponse = client.GetStringAsync("https://api.kanye.rest").Result;
+                var ronResponse = client.GetStringAsync("https://ron-swanson-quotes.herokuapp.com/v2/quotes").Result;
+
+                var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                var kanyeQuote = JObject.Parse(kanyeResponse).ToString().Replace('{', ' ').Replace('}', ' ').Trim();
+
+                Console.WriteLine($"Ron Quote: {ronQuote}");
+                Console.WriteLine($"Kanye Quote: {kanyeQuote}");
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+            
+                
+
+            
+        }
+    }
+}
